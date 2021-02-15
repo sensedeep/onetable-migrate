4fedd737725ed7a5e8da147d97386dab8fd319e9
Michael O'Brien <mob@sensedeep.com>
2021-02-15 16:29:34 +1100

    DEV: support remote proxy invocation
    * Breaking changes 
    * getCurrentVersion and getOutstandingVersions are now async
    * Removed init() and update()
    * Migrate schema changed PK & SK to permit find by query vs scan

--- src/index.js
+++ src/index.js
@@ -7,11 +7,9 @@ import Path from 'path'
 import Semver from 'semver'
 import { Model, Table } from 'dynamodb-onetable'
 
-const SemVerExp = /^([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$/
-
 const MigrationFields = {
-    pk:             { value: '_migrations:${version}' },
-    sk:             { value: '_migrations:' },
+    pk:             { value: '_migrations:' },
+    sk:             { value: '_migrations:${version}' },
     description:    { type: String, required: true },
     date:           { type: Date, required: true },
     path:           { type: String, required: true },
@@ -20,6 +18,12 @@ const MigrationFields = {
 
 export default class Migrate {
 
+    /*
+        params: {
+            migrations: [in-memory-migrations]
+            dir: 'migrations-directory'
+        }
+     */
     constructor(db, params = {}) {
         this.params = params
         this.db = db
@@ -28,27 +32,22 @@ export default class Migrate {
         this.Migration = new Model(db, '_Migration', { fields: MigrationFields }, {timestamps: false})
     }
 
-    async init() {
-        await this.update()
-    }
-
-    async update() {
-        this.pastMigrations = await this.Migration.scan()
-        this.sortMigrations(this.pastMigrations)
-    }
-
+    /* public */
     async findPastMigrations() {
-        return this.pastMigrations
+        let pastMigrations = await this.Migration.find()
+        this.sortMigrations(pastMigrations)
+        return pastMigrations
     }
 
+    /* public */
     async apply(direction, version) {
         let migration = await this.loadMigration(version)
         if (direction == 0) {
-            let outstanding = this.getOutstandingVersions()
+            let outstanding = await this.getOutstandingVersions()
             if (outstanding.length) {
                 version = outstanding[outstanding.length - 1]
             } else {
-                version = this.getCurrentVersion()
+                version = await this.getCurrentVersion()
             }
             migration.version = version
         }
@@ -57,33 +56,64 @@ export default class Migrate {
             await this.Migration.remove({version: migration.version})
         } else {
             await migration.task.up(this.db, this)
-            await this.Migration.create({
+            let params = {
                 version,
                 date: new Date(),
                 path: migration.path,
                 description: migration.description,
-            }, {exists: null})
+            }
+            await this.Migration.create(params)
         }
         return migration
     }
 
+    /* public */
+    async getCurrentVersion() {
+        let pastMigrations = await this.findPastMigrations()
+        if (pastMigrations.length == 0) {
+            return '0.0.0'
+        }
+        return pastMigrations[pastMigrations.length - 1].version
+    }
+
+    /*
+        Return outstanding versions in semver sorted order up to the specified limit
+     */
+    /* public */
+    async getOutstandingVersions(limit = Number.MAX_SAFE_INTEGER) {
+        let current = await this.getCurrentVersion()
+        let pastMigrations = await this.findPastMigrations()
+        let versions
+        if (this.migrations) {
+            versions = this.migrations.map(m => m.version)
+        } else {
+            versions = Fs.readdirSync(this.dir).map(file => file.replace(/\.[^/.]+$/, '')).filter(version => {
+                return Semver.valid(version) && Semver.compare(version, current) > 0
+            }).sort(Semver.compare)
+        }
+        versions = versions.filter(v => pastMigrations.find(m => m.version == v) == null)
+        return versions.slice(0, limit)
+    }
+
+    /* private */
     async loadMigration(version) {
         let path, task
         if (this.migrations) {
-            task = this.migrations.find(m => m.version == version)
-            path = 'memory'
+            let migration = this.migrations.find(m => m.version == version)
+            task = migration.task
         } else {
             path = `${this.dir}/${version}.js`
             task = (await import(path)).default
         }
         return {
-            version,
             description: task.description,
-            path,
+            path: path || 'memory',
+            version,
             task,
         }
     }
 
+    /* private */
     sortMigrations(array) {
         array.sort((a, b) => {
             let cmp = Semver.compare(a.version, b.version)
@@ -99,29 +129,5 @@ export default class Migrate {
                 return 0
             }
         })
-    }
-
-    getCurrentVersion() {
-        if (this.pastMigrations.length == 0) {
-            return '0.0.0'
-        }
-        return this.pastMigrations[this.pastMigrations.length - 1].version
-    }
-
-    /*
-        Return outstanding versions in semver sorted order up to the specified limit
-     */
-    getOutstandingVersions(limit = Number.MAX_SAFE_INTEGER) {
-        let current = this.getCurrentVersion()
-        let versions
-
-        if (this.migrations) {
-            versions = this.migrations.map(m => m.version)
-        } else {
-            versions = Fs.readdirSync(this.dir).map(file => file.replace(/\.[^/.]+$/, '')).filter(version => {
-                return Semver.valid(version) && Semver.compare(version, current) > 0 && this.pastMigrations.find(m => m.version == version) == null
-            }).sort(Semver.compare)
-        }
-        return versions.slice(0, limit)
     }
 }
